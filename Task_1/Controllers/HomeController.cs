a0f86668783d1ffc0c99e4c228b39556918ebc2c
Emrecan <emrecanyasar8@outlook.com>
2022-11-29 21:30:15 +0300

    Add project files.

---
+++ Task_1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Task_1.Models;
+using System.Linq;
+using Task_1.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task_1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            NORTHWNDContext context = new NORTHWNDContext();
+            List<Order> ordersList = context.Orders.ToList();
+            return View(ordersList);
+        }
+    }
+}
---
+++ Task_1/DTO/OrderDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Task_1.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public DateTime? OrderDate { get; set; }
+    }
+}
